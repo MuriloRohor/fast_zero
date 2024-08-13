b7cccc8fb02a473996b4fd45bf7c15758d255437
Murilo Rohor <114838911+MuriloRohor@users.noreply.github.com>
2024-08-13 19:29:59 -0300

    🔬 test: adicionando testes para erros no security e na rota auth - testando no login de autenticação erros para senha ou email errados - cobrindo exceções para o get_current_user

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 
-def test_get_token(client, user):
+def test_login_for_access_token(client, user):
     response = client.post(
         "/auth/token/",
         data={"username": user.email, "password": user.clean_password},
@@ -11,3 +11,29 @@ def test_get_token(client, user):
     assert response.status_code == HTTPStatus.OK
     assert token["token_type"] == "Bearer"
     assert "access_token" in token
+
+
+def test_login_for_access_token_email_incorrect(client, user):
+    response = client.post(
+        "/auth/token/",
+        data={
+            "username": "email_errado@teste.com",
+            "password": user.clean_password,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Incorrect email or password"}
+
+
+def test_login_for_access_token_password_incorrect(client, user):
+    response = client.post(
+        "/auth/token/",
+        data={
+            "username": user.email,
+            "password": "senha_errada"
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Incorrect email or password"}
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
 
+import pytest
+from fastapi.exceptions import HTTPException
 from jwt import decode
 
-from fast_zero.security import create_access_token, settings
+from fast_zero.security import create_access_token, get_current_user, settings
 
 
 def test_jwt():
@@ -24,3 +26,17 @@ def test_jwt_invalid_token(client):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {"detail": "Could not validate credentials"}
+
+
+def test_get_current_no_sub(user):
+    token = create_access_token({"no_sub": "teste@teste.com"})
+
+    with pytest.raises(HTTPException):
+        get_current_user(user, token)
+
+
+def test_get_current_user_not_found(session):
+    token = create_access_token({"sub": "t2este@teste.com"})
+
+    with pytest.raises(HTTPException):
+        get_current_user(session, token)
